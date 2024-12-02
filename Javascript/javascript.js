c3affe17ff851997875a2442fc5c932ca5c65a24
Thecodingpanda <th3codingpanda@gmail.com>
2024-12-02 18:36:27 +0100

    did a little fixing
    🙏

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -14,9 +14,9 @@ const number_rolled = [
   "images/six_eyes.png",
 ];
 let times_rolled = 0;
-const upper_section = [0, 0, 0, 0, 0, 0, false];
+const upper_section = [][0, 0, 0, 0, 0, 0, false];
 // 1 2 3 4 5 6 check if score is 63 or above and reward 35 points
-const lower_section = [0, 0, false, false, false, , false, 0, false];
+const lower_section = [][0, 0, false, false, false,false, 0, false];
 /* 3 of a kind, 4 of a kind ,3 and 2 of a kind , sequence of 4 (1234/2345/3456), 
 sequence of 5(12345/23456) 5 of a kind, chance any dice added together,
  5 of a kind if 5 of a kind is filled */
@@ -90,7 +90,7 @@ function update() {
 }
 function selectplayersamount() {
   numberplayersselected = document.getElementById("inputtext1").value;
-  if (isNaN(numberplayersselected)|| numberplayersselected<= 0) {
+  if (isNaN(numberplayersselected) || numberplayersselected <= 0) {
     alert("Input a valid number");
     console.log(numberplayersselected);
   } else {
@@ -124,4 +124,9 @@ function darklightmodestartup() {
     console.log("goku");
     document.getElementById("dark-lightmode").checked = false;
   }
+}
+function scoreboardupdate(){
+  for(let i = 0; i<7; i++){
+console.log(upper_section[playerturn[i]])
+}
 }
